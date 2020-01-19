3ca57f17b7b6100d2b6a5bf17ddc4a1ceefbb5a1
Rahul Pandey <rahul.office80@gmail.com>
2020-01-19 16:15:47 -0500

    added meeting rooms

---
+++ Leetcode/src/MergeIntervals.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class MergeIntervals {
+
+  static int merge_intervals(int[][] inputArray){
+
+    int rooms =0;
+
+    int[] start = new int[inputArray.length];
+    int[] end = new int[inputArray.length];
+
+    for(int k=0; k < inputArray.length; k++){
+      start[k] = inputArray[k][0];
+      end[k] = inputArray[k][1];
+    }
+    int endPosition = 0;
+    Arrays.sort(start);
+    Arrays.sort(end);
+    for(int j = 0; j < inputArray.length; j++) {
+        if(start[j] < end[endPosition]){
+           rooms++;
+        } else {
+           endPosition++;
+        }
+    }
+    return rooms;
+  }
+
+  public static void main(String[] args){
+//    int[][] input = {{0,30},{5,10},{15,20}};
+    int[][] input = {{7,10},{2,4}};
+    System.out.println(merge_intervals(input));
+  }
+}
---
+++ Leetcode/src/NextClosestTime.java
@@ -0,0 +1,11 @@
+public class NextClosestTime {
+
+  public String nextClosestTime(String time) {
+
+    return "";
+  }
+
+  public static void main(String[] args){
+
+  }
+}
