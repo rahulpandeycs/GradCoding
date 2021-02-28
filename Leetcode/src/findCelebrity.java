9a451bd3b452c773fabd3431b4aa9022d1c35961
RAHUL PANDEY <rahul.office80@gmail.com>
2021-02-28 15:51:27 -0500

    Update findCelebrity.java

--- Leetcode/src/findCelebrity.java
+++ Leetcode/src/findCelebrity.java
@@ -4,7 +4,7 @@
 
 // You are given a helper function bool knows(a, b) which tells you whether A knows B. Implement a function int findCelebrity(n). There will be exactly one celebrity if he/she is in the party. Return the celebrity's label if there is a celebrity in the party. If there is no celebrity, return -1.
 
- 
+// https://leetcode.com/problems/find-the-celebrity/
 // Solution:
 
 /* The knows API is defined in the parent class Relation.
